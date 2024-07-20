bd509aae50e85eeb3a28c3ebdb4522c75e7457ee
Denisoed <denisoed@gmal.com>
2024-07-20 10:12:50 +0600

    Created useColor composable

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,14 @@
 </template>
 
 <script setup>
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, onBeforeMount, ref } from 'vue';
+import useColor from '@/composables/useColor';
+
 import SideBar from '@/components/SideBar.vue';
+
+const { getRandomGradient, rgbToHex } = useColor();
+
+const currentGradient = ref();
 
 function vibrate() {
   if (window?.Telegram?.WebApp) {
@@ -13,36 +19,18 @@ function vibrate() {
   }
 }
 
-function getRandomGradient() {
-  const gradients = [
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(255, 127, 80) 84%)", // коралловый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(240, 128, 128) 84%)", // светло-коралловый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(255, 160, 122) 84%)", // светло-лососевый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(250, 128, 114) 84%)", // лососевый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(255, 99, 71) 84%)", // помидорный
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(255, 69, 0) 84%)", // оранжево-красный
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(255, 165, 0) 84%)", // оранжевый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(255, 215, 0) 84%)", // золотой
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(124, 252, 0) 84%)", // зелено-желтый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(127, 255, 0) 84%)", // липовый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(173, 255, 47) 84%)", // желтовато-зеленый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(0, 255, 127) 84%)", // весенне-зеленый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(0, 250, 154) 84%)", // средний весенне-зеленый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(0, 255, 255) 84%)", // циан
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(64, 224, 208) 84%)", // бирюзовый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(72, 209, 204) 84%)", // средний бирюзовый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(0, 206, 209) 84%)", // темно-бирюзовый
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(70, 130, 180) 84%)", // стальной синий
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(100, 149, 237) 84%)", // васильковый синий
-    "linear-gradient(0deg, rgb(190, 190, 190) 0%, rgb(123, 104, 238) 84%)"  // средний фиолетовый
-  ];
-  
-  const randomIndex = Math.floor(Math.random() * gradients.length);
-  return gradients[randomIndex];
-}
+onBeforeMount(() => {
+  currentGradient.value = getRandomGradient();
+  if (window?.Telegram?.WebApp) {
+    window.Telegram.WebApp.expand()
+    window.Telegram.WebApp.setBackgroundColor(rgbToHex(currentGradient.value))
+    window.Telegram.WebApp.setHeaderColor(rgbToHex(currentGradient.value))
+    window.Telegram.WebApp.ready()
+  }
+})
 
 onMounted(() => {
-  document.body.style.background = getRandomGradient();
+  document.body.style.background = `linear-gradient(0deg, rgb(190, 190, 190) 0%, ${currentGradient.value} 84%)`;
   document.addEventListener('click', vibrate);
 })
 
@@ -50,18 +38,3 @@ onUnmounted(() => {
   document.removeEventListener('click', vibrate);
 })
 </script>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/composables/useColor.ts
@@ -0,0 +1,46 @@
+const useColor = () => {
+  function getRandomGradient() {
+    const gradients = [
+      "rgb(255, 127, 80)", // коралловый
+      "rgb(240, 128, 128)", // светло-коралловый
+      "rgb(255, 160, 122)", // светло-лососевый
+      "rgb(250, 128, 114)", // лососевый
+      "rgb(255, 99, 71)", // помидорный
+      "rgb(255, 69, 0)", // оранжево-красный
+      "rgb(255, 165, 0)", // оранжевый
+      "rgb(255, 215, 0)", // золотой
+      "rgb(124, 252, 0)", // зелено-желтый
+      "rgb(127, 255, 0)", // липовый
+      "rgb(173, 255, 47)", // желтовато-зеленый
+      "rgb(0, 255, 127)", // весенне-зеленый
+      "rgb(0, 250, 154)", // средний весенне-зеленый
+      "rgb(0, 255, 255)", // циан
+      "rgb(64, 224, 208)", // бирюзовый
+      "rgb(72, 209, 204)", // средний бирюзовый
+      "rgb(0, 206, 209)", // темно-бирюзовый
+      "rgb(70, 130, 180)", // стальной синий
+      "rgb(100, 149, 237)", // васильковый синий
+      "rgb(123, 104, 238)"  // средний фиолетовый
+    ];
+    
+    const randomIndex = Math.floor(Math.random() * gradients.length);
+    return gradients[randomIndex];
+  }
+
+  function rgbToHex(rgb: string) {
+    const rgbArray = rgb.replace(/[^\d,]/g, "").split(',');
+    const r = parseInt(rgbArray[0]);
+    const g = parseInt(rgbArray[1]);
+    const b = parseInt(rgbArray[2]);
+    const hex = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+
+    return hex;
+  }
+
+  return {
+    getRandomGradient,
+    rgbToHex
+  }
+};
+
+export default useColor;
