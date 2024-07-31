d8c3363d7f7cf53def027f786fc2b36e77ae2ba8
Denisoed <denisoed@gmal.com>
2024-07-31 19:51:21 +0600

    Minor fixes

--- src/boots/telegram.ts
+++ src/boots/telegram.ts
@@ -10,8 +10,7 @@ export default async () => {
 
   const WebApp = window?.Telegram?.WebApp;
   if (WebApp) {
-    const aaa = '{"query_id":"AAEGKqcZAAAAAAYqpxnIfeGM","user":{"id":430385670,"first_name":"Denis","last_name":"Grushkin","username":"denisoed","language_code":"en","allows_write_to_pm":true},"auth_date":"1722424584","hash":"d65f2aae21b21eba967e4e519f040e6058a3c89f7da73c8d7267355d3f775d7d"}'
-    const data = await telegramAuth(JSON.parse(aaa) || WebApp?.initDataUnsafe);
+    const data = await telegramAuth(WebApp?.initDataUnsafe);
     if (data?.user) {
       localStorage.setItem(ACCESS_TOKEN_LOCAL_STORAGE_KEY, String(data.jwt));
       userStore.setUser(data.user || {});
