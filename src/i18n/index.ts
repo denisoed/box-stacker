1d533446737b1c92e9d4926c318dde034735869b
Denisoed <denisoed@gmal.com>
2024-08-02 20:16:44 +0600

    Added auto detect locale

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,11 +2,13 @@ import { createI18n } from 'vue-i18n';
 import en from '@/i18n/locales/en.json'; 
 import ru from '@/i18n/locales/ru.json'; 
 
+const locale = window?.Telegram?.WebApp?.initDataUnsafe?.user?.language_code || 'en';
+
 const i18n = createI18n({ 
-  locale: navigator.language,
+  locale,
   fallbackLocale: 'en', 
   messages: { ru, en }, 
   legacy: false 
-})
+});
 
-export default i18n
+export default i18n;
