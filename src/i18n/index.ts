0b5dffc7bd931cc54c951f8a2e85356367663d85
Denisoed <denisoed@gmal.com>
2024-08-29 22:50:43 +0600

    Minor fix

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,7 +2,7 @@ import { createI18n } from 'vue-i18n';
 import en from '@/i18n/locales/en.json'; 
 import ru from '@/i18n/locales/ru.json'; 
 
-const locale = 'en' || window?.Telegram?.WebApp?.initDataUnsafe?.user?.language_code || 'en';
+const locale = window?.Telegram?.WebApp?.initDataUnsafe?.user?.language_code || 'en';
 
 const i18n = createI18n({ 
   locale,
