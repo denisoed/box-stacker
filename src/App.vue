677aeb825f3710f400c7982621e2cc723cf43127
Denisoed <denisoed@gmal.com>
2024-09-04 22:15:00 +0600

    Removed QrCode layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <template>
-  <AppLayout v-if="isAndroid || isIOS" />
-  <QrCodeLayout v-else />
+  <AppLayout />
 </template>
 
 <script setup>
-import UAParser from 'ua-parser-js';
 import AppLayout from '@/layouts/AppLayout.vue';
-import QrCodeLayout from '@/layouts/QrCodeLayout.vue';
-
-const UA = new UAParser();
-const os = UA.getOS();
-
-const isAndroid = os?.name === 'Android';
-const isIOS = os?.name === 'iOS';
 </script>
