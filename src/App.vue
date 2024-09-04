a2aeac6a9c19fdea1583161bb564958f3d98c9a7
Denisoed <denisoed@gmal.com>
2024-09-04 22:01:34 +0600

    Added qa page

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,18 @@
 <template>
-  <SkyStars />
-  <router-view v-slot="{ Component }" class="page">
-    <keep-alive :include="['GamePage']">
-      <component :is="Component" />
-    </keep-alive>
-  </router-view>
-  <Footer />
-  <JenesiusModal />
+  <AppLayout v-if="isAndroid || isIOS" />
+  <QrCodeLayout v-else />
 </template>
 
 <script setup>
-import { onMounted, onUnmounted, onBeforeMount, ref } from 'vue';
-import { VIBRATION_LOCAL_STORAGE_KEY } from '@/config';
-import {
-  container as JenesiusModal,
-  config as configDialog,
-  openModal
-} from 'jenesius-vue-modal';
-import canvasConfetti from 'canvas-confetti';
-import useUserApi from '@/api/useUserApi';
-import useUser from '@/composables/useUser';
+import UAParser from 'ua-parser-js';
+import AppLayout from '@/layouts/AppLayout.vue';
+import QrCodeLayout from '@/layouts/QrCodeLayout.vue';
 
-import Footer from '@/components/Footer.vue';
-import SkyStars from '@/components/SkyStars.vue';
-import DailyRewardDialog from '@/components/Dialogs/DailyRewardDialog.vue';
+const UA = new UAParser();
+const os = UA.getOS();
 
-configDialog({
-  animation: "slide-bottom"
-})
+const isAndroid = os?.name === 'Android';
+const isIOS = os?.name === 'iOS';
 
-const { checkDailyReward, collectDailyReward } = useUserApi();
-const { fetchUser } = useUser();
-
-const currentGradient = ref();
-
-function vibrate() {
-  const vibrationIsEnabled = localStorage.getItem(VIBRATION_LOCAL_STORAGE_KEY) === 'true';
-  if (window?.Telegram?.WebApp && vibrationIsEnabled) {
-    window.Telegram.WebApp.HapticFeedback.selectionChanged();
-  }
-}
-
-async function onOpenDailyRewardDialog(reward) {
-  const modal = await openModal(DailyRewardDialog, {
-    reward
-  })
-  canvasConfetti({
-    spread: 70,
-    origin: { y: 1.2 }
-  });
-  modal.on('on-collect', async () => {
-    await collectDailyReward();
-    fetchUser();
-    canvasConfetti({
-      spread: 70,
-      origin: { y: 1.2 }
-    });
-    modal.close();
-  })
-  modal.on('close', () => {
-    modal.close();
-  })
-}
-
-async function checkReward() {
-  const response = await checkDailyReward();
-  if (response?.data?.success) {
-    onOpenDailyRewardDialog(response.data.reward);
-  }
-}
-
-onBeforeMount(() => {
-  currentGradient.value = 'rgb(240, 128, 128)';
-  checkReward();
-})
-
-onMounted(() => {
-  document.body.style.background = `linear-gradient(0deg, rgb(190, 190, 190) 0%, ${currentGradient.value} 84%)`;
-  document.addEventListener('click', vibrate);
-})
-
-onUnmounted(() => {
-  document.removeEventListener('click', vibrate);
-})
+console.log(isAndroid, isIOS);
 </script>
-
-<style lang="scss" scoped>
-.page {
-  position: relative;
-  z-index: 1;
-}
-</style>
